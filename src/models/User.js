a743a66d372c03b781b3bf3b8562b41194d9fb05
Confidence Emonena <confidinho@yahoo.com>
2026-03-21 14:15:12 +0100

    cron updat

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
 	fullName: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
+	phoneNumber: {
+		type: String,
+		default: null,
+		trim: true,
+	},
 	password: {
 		type: String,
 		required: function () {
